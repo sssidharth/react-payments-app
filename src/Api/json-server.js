85be0ddf57b4a63028d0ac96d557661b50748d1c
sssidharth <sssidharth126@gmail.com>
2024-12-23 09:14:58 +0530

    Deployment updated

--- src/Api/json-server.js
+++ src/Api/json-server.js
@@ -1,15 +1,9 @@
-const { createServer } = require("json-server");
-const server = createServer();
-const router = require("./db.json");
-const middlewares = require("json-server").defaults();
-
-server.use((req, res, next) => {
-    res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
-    res.setHeader("Expires", "0");
-    res.setHeader("Pragma", "no-cache");
-    next();
-  });
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json"); // Ensure the path is correct
+const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
+
 module.exports = server;
